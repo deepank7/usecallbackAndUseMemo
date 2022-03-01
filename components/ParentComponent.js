bd5eb48ca3425d310583f437087017c2ff2c8975
Deepank Sharma <deepank@wisdomleaf.com>
2022-03-01 13:54:48 +0530

    Simple implementation for useCallback and useMemo

---
+++ components/ParentComponent.js
@@ -0,0 +1,24 @@
+import React, { useCallback, useState } from "react";
+import MyList from "./MyList";
+export default function ParentComponent() {
+  const [state, setState] = useState(false);
+  const [dep] = useState(false);
+  console.log("Parent Component redered");
+
+  const handler = useCallback(
+    (event) => {
+      console.log("You clicked ", event.currentTarget);
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [dep]
+  );
+  const statehanddler = () => {
+    setState(!state);
+  };
+  return (
+    <>
+      <button onClick={statehanddler}>Change State Of Parent Component</button>
+      <MyList handler={handler} />
+    </>
+  );
+}
